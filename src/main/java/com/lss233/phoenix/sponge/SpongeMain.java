3fcab505ba40126611b2ff7fb86de4b067df2ffa
lss233 <8984680+lss233@users.noreply.github.com>
2017-12-30 11:28:08 +0800

    Initial project structure

---
+++ src/main/java/com/lss233/phoenix/sponge/SpongeMain.java
@@ -0,0 +1,10 @@
+package com.lss233.phoenix.sponge;
+
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ *
+ */
+@Plugin(id = "PhoenixSponge", name = "Phoenix Sponge Plugin", version = "1.0")
+public class SpongeMain {
+}
